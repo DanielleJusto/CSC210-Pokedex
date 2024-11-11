4046c0b73c9158cb8b4d255513e8d8b31332f318
Danielle Justo <144483735+DanielleJusto@users.noreply.github.com>
2024-11-11 17:21:54 -0500

    Add files via upload

---
+++ pokedex/pokemon/src/pokeButton.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class pokeButton extends JButton {
+    /**
+     * constructor for buttons in GUI
+     * 
+     * @param text text inside the button!
+     */
+    public pokeButton(String text) {
+        super(text);
+        setFont(new Font("Monospace 821 BT", Font.PLAIN, 18));
+
+    }
+}
